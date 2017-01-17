4aeea1b452d88987ae5556270622bcefba891dae
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-01-17 17:35:55 +0100

    Conditional if

---
+++ example06.py
@@ -0,0 +1,16 @@
+x = 2
+y = 7
+z = 10
+
+if x > y:
+    print(x, 'is greater than ', y)
+if x < y:
+    print(x, 'is less than ', y)
+if x == y:
+    print(x, 'is the same as', y)
+
+#if x == '2';
+#    print(x, 'equals the string 2')
+
+if x <= y:
+    print(x, 'is less than or equal to', y)
