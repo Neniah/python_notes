2ee785cd260b67afa8b239ace8b6492e46ae4c06
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-01-19 13:34:30 +0100

    Arg optional or not

--- example10.py
+++ example10.py
@@ -3,4 +3,18 @@ def addition(num1, num2):
     return answer
 
 x = addition(5, 6)
-print x 
+print x
+
+def website(font, background_color, fontsize, font_color):
+    print('font:', font)
+    print('bg:', background_color)
+    print('Font size: ', fontsize)
+    print('Font color:', font_color)
+
+
+website('TNR', 'white', '11', 'black')
+
+website(background_color='white',
+        fontsize='11',
+        font_color='black',
+        font='TNR')
