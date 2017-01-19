cfdf937cc92867644f8e1dcefb1eef9e1e0814f7
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-01-19 11:39:25 +0100

    And , or

--- example08.py
+++ example08.py
@@ -2,9 +2,9 @@ x = 3
 y = 7
 z = 10
 
-if x > y:
+if x > y and x < z:
     print('x is greater than y')
-elif x  < z:
+elif x  < z or y < z:
     print('x is less than z')
 else:
     print('nothing was the case')
