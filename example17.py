fa9ee968dffa4c4b698742197b3c7c0655536fc8
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-01-27 09:29:55 +0100

    Mean, median, mode

---
+++ example17.py
@@ -0,0 +1,17 @@
+# Input from User:
+'''
+name = input('What is your name?: ')
+print('Hello', name)
+'''
+import statistics
+
+exList = [4,3,4,6,1,7,8,5,9 ]
+
+x = statistics.mean(exList)
+print(x)
+
+x = statistics.median(exList)
+print(x)
+
+x = statistics.mode(exList)
+print(x)
