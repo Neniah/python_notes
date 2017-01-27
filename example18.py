48800bd6138ac81c0948a0aac104f158d3225667
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-01-27 09:38:33 +0100

    So many ways

--- example18.py
+++ example18.py
@@ -1,8 +1,9 @@
-from statistics import mean as m
+#from statistics import mean as m, stdev as s
 #import statistics as s
-
+from statistics import *
 exList = [5,6,7,8,4,5,3,1,2,3,4,5,6,9,5]
 
 #print(s.mean(exList))
 
 print(mean(exList))
+print(stdev(exList))
